6d84c5063b25eb15fb2922a2a1d21c7a340d4a91
Shubham Mishra <shubham@primedigital.tech>
2021-04-29 17:16:27 +0530

    config for static files

--- teams_be/urls.py
+++ teams_be/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
 
 from teams_be.frontend import urls
 from teams_be import api_urls, frontend
@@ -11,9 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(api_urls)),
     path('', include(frontend.urls)),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
