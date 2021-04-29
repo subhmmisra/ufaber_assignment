4b72f9c678b76e2d7b2570e271447f0c47835957
Shubham Mishra <shubham@primedigital.tech>
2021-04-29 17:35:23 +0530

    static file config

--- teams_be/settings.py
+++ teams_be/settings.py
@@ -152,11 +152,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticFiles')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #this is not used
+# Add static folder to STATIC_DIRS
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
