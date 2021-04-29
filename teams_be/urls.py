99c82f52fc9cf0d2ba8cee6c19688682d336f0ac
Shubham Mishra <shubham@primedigital.tech>
2021-04-29 16:57:50 +0530

    fix: remove errors

--- teams_be/urls.py
+++ teams_be/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-]
 
 
 
